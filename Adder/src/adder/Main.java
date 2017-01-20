97fd14a4b18b05bf6603fcb2734757acc913bda7
Nicklaus McClendon <nhm150030@utdallas.edu>
2017-01-20 00:00:00 +0000

    Made error handling messages more specific

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -3,11 +3,15 @@ package adder;
 public class Main {
 
     public static void main(String[] args) {
+		if (args.length == 0) {
+			System.err.println("Please provide integers to add");
+			System.exit(1);
+		}
         try {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide integers to add");
+			System.err.println("Invalid character passed");
         }
     }
 
